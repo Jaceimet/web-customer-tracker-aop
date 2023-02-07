bb6dd92647fb0cde01f3e716b82689958cc86a5d
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2023-02-06 17:54:25 -0800

    Created Aspect
    Added logging support

---
+++ web-customer-tracker-aop/src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -0,0 +1,29 @@
+package com.luv2code.springdemo.aspect;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.annotation.Aspect;
+
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+
+	// setup logger
+	private Logger myLogger = Logger.getLogger(getClass().getName());
+	
+	// setup pointcut declaration
+	
+	
+	// add @Before advice
+	
+	
+	// add @After advice
+	
+	
+	
+	
+	
+	
+}
